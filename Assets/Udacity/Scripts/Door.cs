1de0983defc66501547957055cb7330f2d549c78
Michael <mike.gr002@gmail.com>
2016-11-13 17:43:11 +0200

    Lights baked, Waypoints and Key (with animation) added

--- Assets/Udacity/Scripts/Door.cs
+++ Assets/Udacity/Scripts/Door.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class Door : MonoBehaviour 
 {
     // Create a boolean value called "locked" that can be checked in Update() 
+	private bool locked = true;
 
     void Update() {
         // If the door is unlocked and it is not fully raised
-            // Animate the door raising up
+        // Animate the door raising up
+
     }
 
     public void Unlock()
     {
         // You'll need to set "locked" to true here
+		if (locked) {
+			// open door
+		}
     }
 }
--- Assets/Udacity/Scripts/Key.cs
+++ Assets/Udacity/Scripts/Key.cs
@@ -5,18 +5,30 @@ using UnityEngine;
 public class Key : MonoBehaviour 
 {
     //Create a reference to the KeyPoofPrefab and Door
+	public GameObject keyObject;
+	public GameObject doorObject;
+
+	void Start() {
+		// Set the animation speed to 0.3
+		Animator keyAnimator = gameObject.GetComponent<Animator> ();
+		keyAnimator.speed = 0.3f;	
+
+		// Set the key color to magenta
+		Renderer objectRenderer = gameObject.GetComponentInChildren<Renderer> ();
+		objectRenderer.material.color = Color.magenta;
+	}	
 
 	void Update()
 	{
 		//Bonus: Key Animation
 	}
-
+		
 	public void OnKeyClicked()
 	{
         // Instatiate the KeyPoof Prefab where this key is located
         // Make sure the poof animates vertically
         // Call the Unlock() method on the Door
         // Destroy the key. Check the Unity documentation on how to use Destroy
-    }
-
+		Debug.Log("Key was clicked");
+	}
 }
