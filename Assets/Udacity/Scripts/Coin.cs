d8fb20636342698c33d55b219bb9ebd3605134e8
Michael <mike.gr002@gmail.com>
2016-11-18 00:45:47 +0200

    Game score UI for collecting coins added

--- Assets/Udacity/Scripts/Coin.cs
+++ Assets/Udacity/Scripts/Coin.cs
@@ -6,13 +6,17 @@ public class Coin : MonoBehaviour
 {
 	public AudioClip audioClip;
 
+	private GameObject gameScore;	// Coins collected
+
 	public void Start() {
-		GetComponent<Animator> ().speed = 0.5f;
+		GetComponent<Animator> ().speed = 0.5f;		// Set animation speed to half
+		gameScore = GameObject.Find ("GameStats");	// Get the game score object
 	}
 
     public void OnCoinClicked() {
 		// Play audio when collected and then destroy the gameobject (coin)
 		AudioSource.PlayClipAtPoint(audioClip, transform.position);
+		gameScore.GetComponent<GameScore> ().SetScore ();	// Update coins counter (score)
 		Destroy (gameObject);
     }
 }
---
+++ Assets/Udacity/Scripts/GameScore.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameScore : MonoBehaviour {
+
+	public Canvas ui;
+
+	private int score = 0;
+	private Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+		scoreText = ui.GetComponentInChildren<Text> ();
+		scoreText.text = "Coins: 0/24";
+	}
+
+	// Update score when a coin was collected
+	public void SetScore() {
+		score++;	// Add one coin to score
+		scoreText.text = "Coins: " + score + "/24";
+	}
+}
